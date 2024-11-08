91056322136d37259816e1df8f2959028def7a98
Nathan Rossow <nathanrossow@gmail.com>
2024-11-07 19:15:25 -0600

    Create regressiontestingdocumentation.py

---
+++ regressiontestingdocumentation.py
@@ -0,0 +1,4 @@
+# 10724  - tested application; uploaded .csv, json and xml data.  
+# box plots and piechart don't display
+# json file shows the valid json in the application display.  this is unesseceary 
+
